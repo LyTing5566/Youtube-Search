bf700699fb2fcae7f5d449d7d33458c777fb656d
LyTing <dce957351@gmail.com>
2022-03-27 02:24:30 +0800

    first committ

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+class App extends React.Component {
+  state = { videos: [], selcetedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("もちまる日記");
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selcetedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selcetedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selcetedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//AIzaSyB5gl_uoNP47Grw83SYQ9ivL0nO0__qWRo  (API)
